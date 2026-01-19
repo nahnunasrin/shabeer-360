6fefdd10910ea20076f53cd85f75dc4128c9bdfb
nahnunasrin <nahnunasrin53@gmail.com>
2026-01-19 22:37:14 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,360 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-room",
+      "name": "living room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.022502607685691345,
+        "pitch": -0.0044207603916159854,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.143953693203356,
+          "pitch": 0.06843196590280343,
+          "rotation": 4.71238898038469,
+          "target": "2-dining-room"
+        },
+        {
+          "yaw": -1.665308289730831,
+          "pitch": -0.03274815681788823,
+          "rotation": 4.71238898038469,
+          "target": "5-bedroom-2-"
+        },
+        {
+          "yaw": -1.4931907456634868,
+          "pitch": -0.06724100800359878,
+          "rotation": 1.5707963267948966,
+          "target": "6-kitchen-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-wash-area-",
+      "name": "wash area ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.27148964618647753,
+        "pitch": -0.012207719856391464,
+        "fov": 1.0283568053266792
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21214024493216854,
+          "pitch": 0.025293066328011093,
+          "rotation": 0,
+          "target": "1-wash-area-"
+        },
+        {
+          "yaw": -0.9120035967830535,
+          "pitch": -0.05306426327893732,
+          "rotation": 1.5707963267948966,
+          "target": "6-kitchen-"
+        },
+        {
+          "yaw": 0.33493237154902644,
+          "pitch": 0.03036597556427978,
+          "rotation": 0,
+          "target": "4-bedroom-1-"
+        },
+        {
+          "yaw": 2.6098360955936375,
+          "pitch": 0.11767534897609977,
+          "rotation": 1.5707963267948966,
+          "target": "2-dining-room"
+        },
+        {
+          "yaw": 1.813959793084944,
+          "pitch": 0.012125232268306974,
+          "rotation": 7.853981633974483,
+          "target": "3-prayer-room"
+        },
+        {
+          "yaw": 1.2548599044852704,
+          "pitch": 0.08733555615501842,
+          "rotation": 10.995574287564278,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": -1.5227098560180181,
+          "pitch": -0.03988805490049607,
+          "rotation": 4.71238898038469,
+          "target": "5-bedroom-2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dining-room",
+      "name": "dining room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.04452370540801098,
+        "pitch": 0.0025030207050473763,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2829811354239489,
+          "pitch": 0.05344809611081125,
+          "rotation": 1.5707963267948966,
+          "target": "3-prayer-room"
+        },
+        {
+          "yaw": -2.117534197635605,
+          "pitch": 0.05908482022495676,
+          "rotation": 6.283185307179586,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": -2.970943225081637,
+          "pitch": 0.005271168349539224,
+          "rotation": 1.5707963267948966,
+          "target": "4-bedroom-1-"
+        },
+        {
+          "yaw": 2.68509514665969,
+          "pitch": -0.02116952270172945,
+          "rotation": 0,
+          "target": "1-wash-area-"
+        },
+        {
+          "yaw": 2.2215724826523937,
+          "pitch": -0.0074452099541950645,
+          "rotation": 1.5707963267948966,
+          "target": "6-kitchen-"
+        },
+        {
+          "yaw": 1.9075682360553419,
+          "pitch": 0.02847970474512529,
+          "rotation": 4.71238898038469,
+          "target": "5-bedroom-2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-prayer-room",
+      "name": "prayer room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3016451262707882,
+        "pitch": 0.00022599720242766352,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1957777064020814,
+          "pitch": 0.08424366030579478,
+          "rotation": 1.5707963267948966,
+          "target": "6-kitchen-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bedroom-1-",
+      "name": "bedroom 1 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.1709710287667825,
+        "pitch": 0.0025030207050473763,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8232610099845097,
+          "pitch": 0.02370097005133509,
+          "rotation": 4.71238898038469,
+          "target": "5-bedroom-2-"
+        },
+        {
+          "yaw": -2.591483666840574,
+          "pitch": 0.13892936821714486,
+          "rotation": 7.853981633974483,
+          "target": "1-wash-area-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bedroom-2-",
+      "name": "bedroom 2 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.23220114146938542,
+        "pitch": -0.007866013357880064,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6620016805488724,
+          "pitch": 0.028442466079281914,
+          "rotation": 4.71238898038469,
+          "target": "1-wash-area-"
+        },
+        {
+          "yaw": 2.8033388475633467,
+          "pitch": -0.0047135213504141404,
+          "rotation": 0,
+          "target": "4-bedroom-1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-kitchen-",
+      "name": "kitchen ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.28826159045493327,
+        "pitch": 0.0005875607622751744,
+        "fov": 1.1700137564469746
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "shabeer katharammal 360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
